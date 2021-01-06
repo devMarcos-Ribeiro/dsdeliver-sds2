34b8d1d85c6d550bba7c54e54e8244492920c26a
Marcos Gabriel Ribeiro Souto <marcosgrsouto@gmail.com>
2021-01-06 19:56:02 -0300

    Add products list based on a API call

--- frontend/front-web/src/Orders/index.tsx
+++ frontend/front-web/src/Orders/index.tsx
@@ -1,11 +1,26 @@
 import './styles.css';
 
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts()
+    .then(response => setProducts(response.data))
+    .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="orders-container">
       <StepsHeader />
+      <ProductsList products={products} />
     </div>
   );
 }
