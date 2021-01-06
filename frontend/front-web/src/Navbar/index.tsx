3d8925d2339098dc68d64d5305f09ec46713ed2c
Marcos Gabriel Ribeiro Souto <marcosgrsouto@gmail.com>
2021-01-06 18:37:56 -0300

    Add routes and Steps Header

--- frontend/front-web/src/Navbar/index.tsx
+++ frontend/front-web/src/Navbar/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css';
 import { ReactComponent as Logo } from '../assets/logo-ds-deliver.svg';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className="main-navbar">
       <Logo />
-      <a href="home" className="logo-text">DS Deliver</a>
+      <Link to="/" className="logo-text">DS Deliver</Link>
     </nav>
   );
 }
