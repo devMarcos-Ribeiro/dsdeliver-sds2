f406b618e02f5bbd5ea5a55ecb75761d1c013958
Marcos Gabriel Ribeiro Souto <marcosgrsouto@gmail.com>
2021-01-09 20:16:34 -0300

    Fixing dot

--- frontend/front-web/src/Home/index.tsx
+++ frontend/front-web/src/Home/index.tsx
@@ -10,7 +10,7 @@ function Home() {
         <div className="home-content">
           <div className="home-actions">
             <h1 className="home-title">
-              Faça seu pedido <br /> que entregamos <br /> para você.
+              Faça seu pedido <br /> que entregamos <br /> para você!
           </h1>
             <h3 className="home-subtitle">
               Escolha seu pedido e em poucos minutos <br /> levaremos em sua porta.
